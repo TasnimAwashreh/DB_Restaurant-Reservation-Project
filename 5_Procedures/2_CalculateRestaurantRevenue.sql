21e634f224391b8dce2b0774091ae8d576c306eb
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-09-28 13:25:16 +0300

    2. Database Function - Calculate Restaurant Revenue

---
+++ 5_Procedures/2_CalculateRestaurantRevenue.sql
@@ -0,0 +1,16 @@
+USE [RestaurantReservationProject]
+GO
+
+CREATE FUNCTION fn_CalculateRevenue (@restaurantId INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @revenue INT;
+    SELECT @revenue = SUM([TotalAmount])
+          FROM [dbo].[Reservations] AS r
+          JOIN Orders AS o
+          ON o.ReservationId = r.ReservationId
+          WHERE [RestaurantId] = @restaurantId
+    RETURN ISNULL(@revenue, 0);
+END
+GO
