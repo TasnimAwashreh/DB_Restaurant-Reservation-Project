a791ee78bb7cfaa75db307797e51b803c9ea8566
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-10-13 12:46:13 +0300

    fixed calculatesalary function

--- 5_Procedures/3_CalculateEmployeeSalary.sql
+++ 5_Procedures/3_CalculateEmployeeSalary.sql
@@ -5,23 +5,26 @@ CREATE OR ALTER FUNCTION fn_CalculateEmployeeSalary (@employeeId INT)
 RETURNS INT
 AS
 BEGIN
-    DECLARE @baseSalary INT;
+    DECLARE @salary INT;
     DECLARE @position INT;
 
-    SELECT @baseSalary = SUM([TotalAmount])
-    FROM [dbo].[Orders]
-    WHERE [EmployeeId] = @employeeId
-
-    SELECT @position =
-        CASE Position
-            WHEN 'VIPOrdersWaiter' THEN 5
-            WHEN 'StandardWaiter'  THEN 4
-            WHEN 'AssistantWaiter' THEN 3
-            ELSE 0
-        END
-    FROM dbo.Employees
-    WHERE EmployeeId = @employeeId;
+    SELECT 
+        @salary =
+            (
+                CASE e.Position
+                    WHEN 'VIPOrdersWaiter' THEN 5
+                    WHEN 'StandardWaiter'  THEN 4
+                    WHEN 'AssistantWaiter' THEN 3
+                    ELSE 0
+                END
+                *SUM([TotalAmount])
+            )
+    FROM dbo.Employees AS e
+    JOIN Orders as o
+    ON o.EmployeeId = e.EmployeeId
+    WHERE e.EmployeeId = @employeeId
+    GROUP BY e.Position;
     
-    RETURN ISNULL(@baseSalary * @position, 0);
+    RETURN ISNULL(@salary, 0);
 END
 GO
