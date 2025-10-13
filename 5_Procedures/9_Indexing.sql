40afac57e09737d4507d76c8fd0fa6d0efaa38a0
Tasnim Awashreh <Tasnim.awashreh@foothillsolutions.com>
2025-10-13 10:38:30 +0300

    removed indexes that are primary keys to remove clutter

--- 5_Procedures/9_Indexing.sql
+++ 5_Procedures/9_Indexing.sql
@@ -2,29 +2,11 @@ USE [RestaurantReservationProject]
 GO
 -- Adding all indexes here
 
--- Restaurant
-CREATE INDEX Restaurant_RestaurantId ON Restaurant(RestaurantId);
-
 -- Order
-CREATE INDEX Order_OrderId ON Orders(OrderId);
 CREATE INDEX Order_OrderDate ON Orders(OrderDate);
 
 -- OrderItems
-CREATE INDEX OrderItems_ItemId ON OrderItems(ItemId);
 CREATE INDEX OrderItems_Quantity ON OrderItems(Quantity);
 
--- MenuItems
-CREATE INDEX MenuItems_ItemId ON MenuItems(ItemId);
-
 -- Reservations
-CREATE INDEX Reservations_ReservationId ON Reservations(ReservationId);
 CREATE INDEX Reservations_ReservationDate ON Reservations(ReservationDate);
-
--- Employees
-CREATE INDEX Employees_EmployeeId ON Employees(EmployeeId);
-
--- Customers
-CREATE INDEX Customers_CustomerId ON Customers(CustomerId);
-
--- Tables
-CREATE INDEX Tables_TableId ON Tables(TableId);
